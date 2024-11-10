71d03d343a07c0ef6b9ea485700168bcb68a1828
kutub6792 <kutub6792@gmail.com>
2024-11-10 20:02:35 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+ let x = ["Pistol","Rifle","Sniper","Ak-47"];
+
+
+ constr = (v)=>{
+     console.log(v);
+ }
+
+ x.forEach(r);
+
+
+ x.forEach((v)=>{
+    console.log(v);
+ });
+
+
+ function arrayPrint(value){
+     console.log(value);
+ }
+
+
+ let arr = [45,22,67,21];
+
+ arr.map((v)=>{
+     console.log(v*10)
+ })
+
+ 
+ let num = [20,10,40,5,,4]
+
+ num.filter((n)=>{
+    console.log(n<18)
+ })
+ 
+
+
+ let x = 5; 
+
+ function f1(){
+    console.log(x);
+ }
+
+ function f2(x=10) {
+    console.log(x);
+ }
+ 
+ f1()
+ f2()
+
+ function x(){
+    let a = "good";
+    console.log("Hi, this is x"+a);
+ }
+
+ function y(){
+    let a="good"
+    console.log("Hi, this is y"+a)
+ }
+    x()
+    y()
+
+
+
+
+
+    let s= " Hello Bangladesh"
+    console.log(s.length);
+    console.log(s.charAt(6));
+    console.log(indexOf("o)"))
+    console.log(substring(7,10))
+    console.log(s.toLowerCase())
+    console.log(toUpperCase())
+    console.log(s.replace("kung","Hi"))
+    console.log(s.trimStart())
+    console.log(trimEnd())
+    console.log(s.trim())
+
+
+    let Arr = [5,4,1,6]  
+
+    function a(v){
+        console.log(v)
+    }
+
+    arr.forEach(a);
+
+    const d = new Data()
+    console.log(d.getFullYear())
+    console.log(d.getDate())
+    condoel.log(d.getMonth()+1)
+    console.log(d.getHours()+":"+d.getMinutes()+":"+d.getsecend())
+
+    d.setFullYear("2023")
+    console.log(d.getFullYear())
+
+    let x =[7,3,1]
+
+    function t(v){
+        console.log(v)
+    }
+
+    x.map(t);
+
+    function clickf (){
+        console.log("Hello Basify")
+        alert("Hello Basify")
+
+    }
+
+ 
+
